9f495c3c3313a727efb648d3ec41695c1f9cefab
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-01-10 03:01:30 +0900

    [CHORE] UI View Extension 수정(#8)

---
+++ Havit/Havit/Global/Extension/UIApplication+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIApplication+Extension.swift
+//  Havit
+//
+//  Created by SHIN YOON AH on 2022/01/10.
+//
+
+import UIKit
+
+extension UIApplication {
+    var keyWindow: UIWindow? {
+        return UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .first(where: { $0 is UIWindowScene })
+            .flatMap({ $0 as? UIWindowScene })?.windows
+            .first(where: \.isKeyWindow)
+    }
+}
--- Havit/Havit/Global/Extension/UIView+Extension.swift
+++ Havit/Havit/Global/Extension/UIView+Extension.swift
@@ -1,5 +1,5 @@
 //
-//  UIView+.swift
+//  UIView+Extension.swift
 //  Havit
 //
 //  Created by SHIN YOON AH on 2022/01/06.
@@ -8,27 +8,25 @@
 import UIKit
 
 extension UIView {
+    @discardableResult
     func makeShadow(_ color: UIColor,
                     _ opacity: Float,
                     _ offset: CGSize,
-                    _ radius: CGFloat) {
+                    _ radius: CGFloat) -> Self {
         layer.shadowColor = color.cgColor
         layer.shadowOpacity = opacity
         layer.shadowOffset = offset
         layer.shadowRadius = radius
+        return self
     }
     
-    @discardableResult
-    func add<T: UIView>(_ subview: T, then closure: ((T) -> Void)? = nil) -> T {
+    func add<T: UIView>(_ subview: T, completionHandler closure: ((T) -> Void)? = nil) {
         addSubview(subview)
         closure?(subview)
-        return subview
     }
     
-    @discardableResult
-    func adds<T: UIView>(_ subviews: [T], then closure: (([T]) -> Void)? = nil) -> [T] {
+    func adds<T: UIView>(_ subviews: [T], completionHandler closure: (([T]) -> Void)? = nil) {
         subviews.forEach { addSubview($0) }
         closure?(subviews)
-        return subviews
     }
 }
