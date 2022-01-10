1b0cf5a2836c3234d0a4775ae95d4d2c41ef0de7
suyeon <rlatndus9911@naver.com>
2022-01-11 02:03:54 +0900

    [ADD] UIFont+Extension 추가(#10)

--- Havit/Havit/Global/Extension/UIFont+Extension.swift
+++ Havit/Havit/Global/Extension/UIFont+Extension.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 enum FontName: String {
-    case dinProRegular = "DINPro-Regular"
+    case pretendardBlack = "Pretendard-Black"
+    case pretendardBold = "Pretendard-Bold"
+    case pretendardExtraBold = "Pretendard-ExtraBold"
+    case pretendardExtraLight = "Pretendard-ExtraLight"
+    case pretendardLight = "Pretendard-Light"
+    case pretendardMedium = "Pretendard-Medium"
+    case pretendardReular = "Pretendard-Regular"
+    case pretendardSemibold = "Pretendard-SemiBold"
+    case pretendardThin = "Pretendard-Thin"
 }
 
 extension UIFont {
