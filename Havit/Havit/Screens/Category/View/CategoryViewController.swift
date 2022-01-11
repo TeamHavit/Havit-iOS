4a83562d484362523517a20b8d2942020925764d
suyeon <rlatndus9911@naver.com>
2022-01-12 04:31:33 +0900

    [FEAT] Category CollectionView 레이아웃 (#21)

--- Havit/Havit/Screens/Category/View/CategoryViewController.swift
+++ Havit/Havit/Screens/Category/View/CategoryViewController.swift
@@ -7,23 +7,85 @@
 
 import UIKit
 
+import SnapKit
+
 class CategoryViewController: UIViewController {
 
+    // MARK: - Vars & Lets Part
+
+    // MARK: - UI Component Part
+
+    private lazy var categoryCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsVerticalScrollIndicator = false
+
+        return collectionView
+    }()
+
+
+    // MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setDelegation()
+        setLayouts()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Custom Method Part
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setDelegation() {
+        categoryCollectionView.delegate = self
+        categoryCollectionView.dataSource = self
     }
-    */
 
+    // MARK: - @objc Function Part
+}
+// MARK: - Extension Part
+
+extension CategoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension CategoryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 343, height: 56)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+extension CategoryViewController {
+    func setLayouts() {
+        setViewHierarchies()
+        setConstraints()
+    }
+
+    func setViewHierarchies() {
+        view.addSubview(categoryCollectionView)
+    }
+
+    func setConstraints() {
+        categoryCollectionView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(148)
+            $0.leading.bottom.trailing.equalToSuperview()
+        }
+    }
 }
