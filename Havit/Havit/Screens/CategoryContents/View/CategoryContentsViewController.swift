6ca8cb200131e68a1747e0fb503b86f35ee4716b
beansBin <beans_bin@naver.com>
2022-01-12 13:25:23 +0900

    [FEAT] CategoryContents view search bar 생성

--- Havit/Havit/Screens/CategoryContents/View/CategoryContentsViewController.swift
+++ Havit/Havit/Screens/CategoryContents/View/CategoryContentsViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class CategoryContentsViewController: UIViewController {
+class CategoryContentsViewController: BaseViewController {
+    var searchController: UISearchController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        setAutoLayouts()
+        
+    }
+    
+    func setUI() {
+        // TODO: 네비게이션바 생성 (메인화면에서 Coordinator로 진입)
+        
+        // 검색바 생성
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "원하는 콘텐츠를 검색하세요."
+        self.navigationItem.searchController = searchController
+        self.navigationItem.title = "카테고리"
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        // TODO: 필터뷰 생성 - 컬렉션 뷰, 보기 버튼, 정렬 버튼
+        
+        
+        
+        // TODO: 메인뷰 생성 - 컬렉션 뷰
+        
+    }
+    
+    func setAutoLayouts() {
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
 
+extension CategoryContentsViewController: UISearchBarDelegate {
+    
 }
