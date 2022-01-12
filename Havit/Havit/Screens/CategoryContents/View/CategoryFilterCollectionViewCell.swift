603c775848f602c6124523b02ddea37b97df1219
beansBin <beans_bin@naver.com>
2022-01-12 19:59:30 +0900

    [ADD] CategoryFilterCollectionViewCell 파일 생성

---
+++ Havit/Havit/Screens/CategoryContents/View/CategoryFilterCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryFilterCollectionViewCell.swift
+//  Havit
+//
+//  Created by 박예빈 on 2022/01/12.
+//
+
+import UIKit
+
+class CategoryFilterCollectionViewCell: UICollectionViewCell {
+    var filterNameLabel: UILabel!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpCell()
+        setUpLabel()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setUpCell()
+        setUpLabel()
+    }
+    
+    func setUpCell() {
+        filterNameLabel = UILabel()
+        contentView.addSubview(filterNameLabel)
+        filterNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        filterNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        filterNameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
+        filterNameLabel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
+        filterNameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
+    func setUpLabel() {
+        // filterNameLabel.font = UIFont.systemFont(ofSize: 32)
+        filterNameLabel.textAlignment = .center
+    }
+}
