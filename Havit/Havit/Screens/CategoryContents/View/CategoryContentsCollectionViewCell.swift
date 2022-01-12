1befe84b8ce91dfdf009fc6b22a9575046c10436
beansBin <beans_bin@naver.com>
2022-01-12 23:05:03 +0900

    [FEAT] CategoryContentsView CategoryContentsCollectionView 구현

--- Havit/Havit/Screens/CategoryContents/View/CategoryContentsCollectionViewCell.swift
+++ Havit/Havit/Screens/CategoryContents/View/CategoryContentsCollectionViewCell.swift
@@ -30,7 +30,7 @@ class CategoryContentsCollectionViewCell: UICollectionViewCell {
         noticeLabel = {
             let label = UILabel()
             label.text = "아직 저장된 콘텐츠가 없습니다."
-            return noticeLabel
+            return label
         }()
         contentView.addSubview(noticeLabel)
         
@@ -44,7 +44,7 @@ class CategoryContentsCollectionViewCell: UICollectionViewCell {
         addButton = {
             let button = UIButton()
             button.setTitle("콘텐츠 추가", for: .normal)
-            return addButton
+            return button
         }()
         contentView.addSubview(addButton)
     }
@@ -52,14 +52,19 @@ class CategoryContentsCollectionViewCell: UICollectionViewCell {
     func setAutoLayouts() {
         noticeLabel.snp.makeConstraints { (make) -> Void in
             make.centerX.equalTo(contentView)
+            make.top.equalTo(contentView).offset(84)
         }
         
         imgView.snp.makeConstraints { (make) -> Void in
             make.centerX.equalTo(contentView)
+            make.left.equalTo(contentView).offset(102)
+            make.top.equalTo(noticeLabel).offset(41)
+            make.width.height.equalTo(172)
         }
         
         addButton.snp.makeConstraints { (make) -> Void in
             make.centerX.equalTo(contentView)
+            make.top.equalTo(imgView).offset(24)
         }
     }
     
