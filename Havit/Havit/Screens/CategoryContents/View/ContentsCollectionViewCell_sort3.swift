8e98352cb087687a951b0e7fe23b6c18e07a0829
beansBin <beans_bin@naver.com>
2022-01-13 14:33:50 +0900

    [FEAT] ContentsCollectionViewCell Layout 구현

---
+++ Havit/Havit/Screens/CategoryContents/View/ContentsCollectionViewCell_sort3.swift
@@ -0,0 +1,12 @@
+//
+//  ContentsCollectionViewCell_sort3.swift
+//  Havit
+//
+//  Created by 박예빈 on 2022/01/12.
+//
+
+import UIKit
+
+class ContentsCollectionViewCell_sort3: UICollectionViewCell {
+    
+}
