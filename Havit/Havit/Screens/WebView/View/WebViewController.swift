cddb6dbae039d1f1addf89c2b182f275136be1c5
sujin <rkdthd1234@naver.com>
2022-01-14 00:41:38 +0900

    [FEAT] AlertPanel 과 새 창 열기 핸들링을 위한 WKUIDelegate 함수 작성 (#32)

--- Havit/Havit/Screens/WebView/View/WebViewController.swift
+++ Havit/Havit/Screens/WebView/View/WebViewController.swift
@@ -15,6 +15,7 @@ final class WebViewController: BaseViewController {
     private lazy var webView: WKWebView = {
         let webView = WKWebView()
         webView.allowsBackForwardNavigationGestures = true
+        webView.uiDelegate = self
         return webView
     }()
     
@@ -46,5 +47,43 @@ final class WebViewController: BaseViewController {
         }
         let urlRequest = URLRequest(url: url)
         webView.load(urlRequest)
+    }
+}
+
+extension WebViewController: WKUIDelegate {
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let okAction = UIAlertAction(title: "확인", style: .default) { (_) in
+            completionHandler()
+        }
+        showAlert(with: message, alertActions: okAction)
+    }
+
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let okAction = UIAlertAction(title: "확인", style: .default) { _ in
+            completionHandler(true)
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .default) { _ in
+            completionHandler(false)
+        }
+        showAlert(with: message, alertActions: okAction, cancelAction)
+    }
+    
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        if let targetFrame = navigationAction.targetFrame,
+           targetFrame.isMainFrame {
+            return nil
+        }
+        
+        // href="_blank" 새 창 열기
+        webView.load(navigationAction.request)
+        return nil
+    }
+    
+    private func showAlert(with message: String, alertActions: UIAlertAction...) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertActions.forEach { alertAction in
+            alertController.addAction(alertAction)
+        }
+        self.present(alertController, animated: true, completion: nil)
     }
 }
