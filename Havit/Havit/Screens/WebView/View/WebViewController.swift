443e492bcd4ffe613e91d25aefa61c694f7b9ac6
sujin <rkdthd1234@naver.com>
2022-01-14 00:00:17 +0900

    [FEAT] WebView 생성 (#32)

--- Havit/Havit/Screens/WebView/View/WebViewController.swift
+++ Havit/Havit/Screens/WebView/View/WebViewController.swift
@@ -5,4 +5,36 @@
 //  Created by SHIN YOON AH on 2022/01/06.
 //
 
-import Foundation
+import UIKit
+import WebKit
+
+final class WebViewController: BaseViewController {
+    
+    // MARK: - property
+    private let url: String
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView()
+        webView.allowsBackForwardNavigationGestures = true
+        return webView
+    }()
+    
+    // MARK: - init
+    init(url: String) {
+        self.url = url
+        super.init()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - life cycle
+    override func loadView() {
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
