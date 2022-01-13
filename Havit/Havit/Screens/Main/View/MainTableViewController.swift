b53cdc936574201707e04ce84dec046af9c961b1
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-01-14 04:00:58 +0900

    [FEAT] TableView Section Layout 구현(#22)

--- Havit/Havit/Screens/Main/View/MainTableViewController.swift
+++ Havit/Havit/Screens/Main/View/MainTableViewController.swift
@@ -11,26 +11,50 @@ import SnapKit
 
 class MainTableViewController: BaseViewController {
     
+    enum Size {
+        static let headerHeight: CGFloat = 50
+        static let footerHeight: CGFloat = 100
+    }
+    
+    enum ReachSection: Int, CaseIterable {
+        case notification
+        case progress
+    }
+    
+    enum CategorySection: Int, CaseIterable {
+        case category
+        case guideline
+        case recent
+        case recommend
+    }
+    
     // MARK: - property
     
     lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.backgroundColor = .clear
+        tableView.sectionHeaderTopPadding = 0
         tableView.register(cell: ReachRateNotificationTableViewCell.self)
         return tableView
     }()
-
-    // MARK: - life cycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 }
 
 extension MainTableViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        switch section {
+        case 0:
+            return ReachSection.allCases.count
+        case 1:
+            return CategorySection.allCases.count
+        default:
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -38,5 +62,43 @@ extension MainTableViewController: UITableViewDataSource {
             withType: ReachRateNotificationTableViewCell.self, for: indexPath)
         
         return cell
+    }
+}
+
+extension MainTableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        switch section {
+        case 1:
+            return MainTopView()
+        default:
+            return UIView()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section {
+        case 1:
+            return Size.headerHeight
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        switch section {
+        case 1:
+            return MainTopView()
+        default:
+            return UIView()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        switch section {
+        case 1:
+            return Size.footerHeight
+        default:
+            return 0
+        }
     }
 }
