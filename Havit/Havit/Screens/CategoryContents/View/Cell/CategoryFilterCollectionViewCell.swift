a350b6060b0078bfd03051fc4c79add31ce476b2
beansBin <beans_bin@naver.com>
2022-01-15 01:16:11 +0900

    [CORRECT] 코드 리뷰 반영 수정

--- Havit/Havit/Screens/CategoryContents/View/Cell/CategoryFilterCollectionViewCell.swift
+++ Havit/Havit/Screens/CategoryContents/View/Cell/CategoryFilterCollectionViewCell.swift
@@ -23,18 +23,18 @@ final class CategoryFilterCollectionViewCell: BaseCollectionViewCell {
     }
     
     override func render() {
-        // filterNameLabel.font = UIFont.systemFont(ofSize: 32)
-        filterNameLabel.textAlignment = .center
-    }
-    
-    override func configUI() {
-        
         // 이 부분은 디자인 부분에서 다시 수정할 예정입니다!!
+        
         contentView.addSubview(filterNameLabel)
         filterNameLabel.translatesAutoresizingMaskIntoConstraints = false
         filterNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
         filterNameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
         filterNameLabel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
         filterNameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
+        filterNameLabel.textAlignment = .center
+    }
+    
+    override func configUI() {
+        // filterNameLabel.font = UIFont.systemFont(ofSize: 32)
     }
 }
