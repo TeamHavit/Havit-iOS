947b3f01e4b9c35b2aaea20d9860235997255513
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-01-14 23:21:14 +0900

    [ADD] Asset 추가(#44) - BaseViewController deinit안으로 removeObserver 이동

--- Havit/Havit/Global/Base/BaseViewController.swift
+++ Havit/Havit/Global/Base/BaseViewController.swift
@@ -16,6 +16,10 @@ class BaseViewController: UIViewController {
     init() {
         super.init(nibName: nil, bundle: nil)
     }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -30,10 +34,6 @@ class BaseViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: false)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        NotificationCenter.default.removeObserver(self)
     }
     
     func render() {
