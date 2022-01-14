f7afa55d316bfd4f06bc36745ecbba80e69781a4
suyeon <rlatndus9911@naver.com>
2022-01-14 20:37:15 +0900

    [CHORE] 코드리뷰 반영

--- Havit/Havit/Screens/Category/View/CategoryListDataModel.swift
+++ Havit/Havit/Screens/Category/View/CategoryListDataModel.swift
@@ -13,18 +13,7 @@ struct CategoryListData {
 }
 
 extension CategoryListData {
-    static let dummy: [CategoryListData] = [
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클1"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클2"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클3"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클4"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클5"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클6"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클7"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클8"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클9"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클10"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클11"),
-        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클12")
-    ]
+    static let dummy: [CategoryListData] = (0...12).map { index in
+        CategoryListData(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클 \(index)")
+    }
 }
