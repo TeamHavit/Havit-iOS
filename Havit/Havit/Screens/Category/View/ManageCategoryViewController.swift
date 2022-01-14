da06be9704d4e3e7ca83b9492a20f2aa58df18c7
suyeon <rlatndus9911@naver.com>
2022-01-14 16:35:19 +0900

    [FEAT] ManageCategoryViewController NavigationBar 세팅 (#42)

--- Havit/Havit/Screens/Category/View/ManageCategoryViewController.swift
+++ Havit/Havit/Screens/Category/View/ManageCategoryViewController.swift
@@ -7,26 +7,67 @@
 
 import UIKit
 
-class ManageCategoryViewController: UIViewController {
+class ManageCategoryViewController: BaseViewController {
 
     // MARK: - property
     
     weak var coordinator: ManageCategoryCoordinator?
 
+    private let backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "iconBackWhite"), for: .normal)
+        return button
+    }()
+
+    private let doneButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("완료", for: .normal)
+        button.titleLabel?.font = .font(.pretendardMedium, ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+
+    // MARK: - life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setNavigationBar()
+        bind()
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func render() {
     }
-    */
 
+    private func setNavigationBar() {
+        title = "카테고리 수정"
+        let appearance = UINavigationBarAppearance()
+
+        appearance.backgroundColor = .havitPurple
+        appearance.titleTextAttributes = [
+            .font: UIFont.font(.pretendardBold, ofSize: 16)
+        ]
+        
+        navigationItem.leftBarButtonItem = makeBarButtonItem(with: backButton)
+        navigationItem.rightBarButtonItem = makeBarButtonItem(with: doneButton)
+    }
+
+    // 이 함수가 앞에 categoryVC에도 똑같이 정의되어있는데 혹시 가져와서 쓸 쑤 있는 방법이 있을까요??
+    private func makeBarButtonItem(with button: UIButton) -> UIBarButtonItem {
+        button.frame = CGRect(x: 0, y: 0, width: 28, height: 28)
+        return UIBarButtonItem(customView: button)
+    }
+
+    private func bind() {
+        backButton.rx.tap
+            .bind(onNext: { [weak self] in
+                self?.coordinator?.performTransition(to: .category)
+            })
+            .disposed(by: disposeBag)
+
+        doneButton.rx.tap
+            .bind(onNext: { [weak self] in
+                self?.coordinator?.performTransition(to: .category)
+            })
+            .disposed(by: disposeBag)
+    }
 }
