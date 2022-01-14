70ce04f64a4bdd8d33ddd31ab7f8d4792642ee4a
suyeon <rlatndus9911@naver.com>
2022-01-15 03:27:18 +0900

    [CHORE] NavigationBar 세팅 코드 수정

--- Havit/Havit/Screens/Category/View/CategoryViewController.swift
+++ Havit/Havit/Screens/Category/View/CategoryViewController.swift
@@ -106,6 +106,7 @@ class CategoryViewController: BaseViewController {
 
     override func configUI() {
         view.backgroundColor = .white
+        setupBaseNavigationBar(backgroundColor: .white, titleColor: .black, shadowImage: UIImage(), isTranslucent: true)
     }
 
     // MARK: - func
@@ -118,9 +119,6 @@ class CategoryViewController: BaseViewController {
         title = "전체 카테고리"
         let font = UIFont.font(.pretendardBold, ofSize: 16)
         navigationController?.navigationBar.titleTextAttributes = [.font: font]
-
-        navigationController?.navigationBar.isTranslucent = false
-        navigationController?.navigationBar.shadowImage = UIImage()
 
         navigationItem.leftBarButtonItem = makeBarButtonItem(with: backButton)
         navigationItem.rightBarButtonItem = makeBarButtonItem(with: editButton)
