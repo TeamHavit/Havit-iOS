6e41553730702c2cfdc597a4982d394684c69e40
suyeon <rlatndus9911@naver.com>
2022-01-17 17:51:52 +0900

    [FEAT] iconCollectionViewCell 추가 (#64)

---
+++ Havit/Havit/Screens/Category/View/CategoryIconCollectionViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  CategoryIconCollectionViewCell.swift
+//  Havit
+//
+//  Created by 김수연 on 2022/01/17.
+//
+
+import UIKit
+import SwiftUI
+
+import SnapKit
+
+class CategoryIconCollectionViewCell: BaseCollectionViewCell {
+
+    // MARK: - property
+
+    private let iconBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray000
+        view.frame = CGRect(x: 0, y: 0, width: 62, height: 62)
+        view.layer.cornerRadius = 31
+        
+        return view
+    }()
+
+    private let iconImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = ImageLiteral.iconCategory
+        image.alpha = 0.7
+        return image
+    }()
+
+    // MARK: - init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - life cycle
+
+    override var isSelected: Bool {
+        didSet {
+            iconImageView.alpha = self.isSelected ? 1 : 0.7
+            iconBackgroundView.layer.borderWidth = self.isSelected ? 1.5 : 0
+            iconBackgroundView.layer.borderColor = UIColor.havitPurple.cgColor
+        }
+    }
+
+    override func render() {
+        self.addSubViews([iconBackgroundView, iconImageView])
+
+        iconBackgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        iconImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(9)
+        }
+    }
+}
