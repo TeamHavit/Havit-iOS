99ffbeef338a7417b730a0ca06ddff90fb75968b
sujin <rkdthd1234@naver.com>
2022-01-18 00:48:16 +0900

    [ADD] zeplin 에 나와있는 정보대로 shadow 를 추가할 수 있는 함수 추가

--- Havit/Havit/Global/Extension/UIView+Extension.swift
+++ Havit/Havit/Global/Extension/UIView+Extension.swift
@@ -20,6 +20,28 @@ extension UIView {
         return self
     }
     
+    @discardableResult
+    func applyZeplinShadow (color: UIColor = .black,
+                            alpha: Float,
+                            x: CGFloat,
+                            y: CGFloat,
+                            blur: CGFloat,
+                            spread: CGFloat) -> Self {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = alpha
+        layer.shadowOffset = CGSize(width: x, height: y)
+        layer.shadowRadius = blur / 2.0
+        if spread == 0 {
+            layer.shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            layer.shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+        return self
+    }
+    
     func addSubView<T: UIView>(_ subview: T, completionHandler closure: ((T) -> Void)? = nil) {
         addSubview(subview)
         closure?(subview)
