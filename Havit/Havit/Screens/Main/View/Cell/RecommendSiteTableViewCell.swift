7b9e1295770bfd80093492b94029c4b25cc185c6
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-01-17 21:27:36 +0900

    [ADD] 셀 내부 구성 완료(#87)

--- Havit/Havit/Screens/Main/View/Cell/RecommendSiteTableViewCell.swift
+++ Havit/Havit/Screens/Main/View/Cell/RecommendSiteTableViewCell.swift
@@ -10,14 +10,41 @@ import UIKit
 import SnapKit
 
 final class RecommendSiteTableViewCell: BaseTableViewCell {
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .red
+    
+    private enum Size {
+        static let cellWidth = { () -> Int in
+            let width = UIScreen.main.bounds.size.width
+            let spacing = 11
+            return Int(width) - spacing * 2
+        }()
+        static let cellHeight = 141
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    // MARK: - property
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "오늘의 추천 사이트"
+        label.font = .font(.pretendardSemibold, ofSize: 17)
+        label.textColor = .primaryBlack
+        return label
+    }()
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "다양한 사이트에서 해빗 할 콘텐츠를 탐색해 보세요"
+        label.font = .font(.pretendardReular, ofSize: 14)
+        label.textColor = .havitGray
+        return label
+    }()
+    private lazy var siteCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.sectionInset = UIEdgeInsets(top: 30, left: 16, bottom: 0, right: 16)
+        flowLayout.itemSize = CGSize(width: Size.cellWidth, height: Size.cellHeight)
+        flowLayout.minimumLineSpacing = 11
+        flowLayout.minimumInteritemSpacing = 11
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        return collectionView
+    }()
     
 }
