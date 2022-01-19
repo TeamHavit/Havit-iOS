db2b625b134fd72611ca0cb8b8d424751930706d
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-01-19 20:58:49 +0900

    [ADD] CollectionView 설정 완료(#120)

--- Havit/Havit/Screens/Main/View/MainUnwatchedViewController.swift
+++ Havit/Havit/Screens/Main/View/MainUnwatchedViewController.swift
@@ -12,6 +12,11 @@ import SnapKit
 
 final class MainUnwatchedViewController: BaseViewController {
     
+    private enum Size {
+        static let cellWidth: CGFloat = UIScreen.main.bounds.size.width
+        static let cellHeight: CGFloat = 139
+    }
+    
     // MARK: - property
     
     weak var coordinator: UnwatchedCoordinator?
@@ -20,6 +25,15 @@ final class MainUnwatchedViewController: BaseViewController {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 28, height: 28))
         button.setImage(ImageLiteral.btnBackBlack, for: .normal)
         return button
+    }()
+    private lazy var contentCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.itemSize = CGSize(width: Size.cellWidth, height: Size.cellHeight)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.dataSource = self
+        collectionView.register(cell: ContentsCollectionViewCell.self)
+        return collectionView
     }()
     private let unwatchedEmptyView = MainUnwatchedEmptyView()
     
@@ -30,8 +44,13 @@ final class MainUnwatchedViewController: BaseViewController {
     
     override func render() {
         view.addSubView(unwatchedEmptyView)
+        unwatchedEmptyView.addSubview(contentCollectionView)
         
         unwatchedEmptyView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        contentCollectionView.snp.makeConstraints {
             $0.top.leading.trailing.bottom.equalToSuperview()
         }
     }
@@ -40,6 +59,7 @@ final class MainUnwatchedViewController: BaseViewController {
         super.configUI()
         view.backgroundColor = .white
         setupNavigationBar()
+        title = "봐야 하는 콘텐츠"
     }
     
     // MARK: - func
@@ -54,17 +74,25 @@ final class MainUnwatchedViewController: BaseViewController {
     }
     
     private func setupNavigationBar() {
-        title = "최근 저장 콘텐츠"
         setupBaseNavigationBar(backgroundColor: .havitWhite,
                                titleColor: .primaryBlack,
                                isTranslucent: false,
                                tintColor: .primaryBlack)
-        let font = UIFont.font(.pretendardBold, ofSize: 16)
-        navigationController?.navigationBar.titleTextAttributes = [.font: font]
         navigationItem.leftBarButtonItem = makeBarButtonItem(with: backButton)
     }
     
     private func makeBarButtonItem(with button: UIButton) -> UIBarButtonItem {
         return UIBarButtonItem(customView: button)
+    }
+}
+
+extension MainUnwatchedViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: ContentsCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+        return cell
     }
 }
