35d217683c3cebb96455633d2063ec696001d744
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-01-20 01:43:06 +0900

    [ADD] MainAlarmViewController 생성(#147)

---
+++ Havit/Havit/Screens/Main/View/MainAlarmViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainAlarmViewController.swift
+//  Havit
+//
+//  Created by SHIN YOON AH on 2022/01/20.
+//
+
+import UIKit
+
+import SnapKit
+
+final class MainAlarmViewController: BaseViewController {
+
+}
