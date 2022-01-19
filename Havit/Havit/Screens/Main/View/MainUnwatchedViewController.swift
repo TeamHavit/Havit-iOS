c5bc42507e21da7af28ae3f16b2c55de3eb88282
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-01-19 21:23:39 +0900

    [ADD] Linespacing 추가(#120)

--- Havit/Havit/Screens/Main/View/MainUnwatchedViewController.swift
+++ Havit/Havit/Screens/Main/View/MainUnwatchedViewController.swift
@@ -30,7 +30,9 @@ final class MainUnwatchedViewController: BaseViewController {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.scrollDirection = .vertical
         flowLayout.itemSize = CGSize(width: Size.cellWidth, height: Size.cellHeight)
+        flowLayout.minimumLineSpacing = 1
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = .gray000
         collectionView.dataSource = self
         collectionView.register(cell: ContentsCollectionViewCell.self)
         return collectionView
@@ -59,7 +61,6 @@ final class MainUnwatchedViewController: BaseViewController {
         super.configUI()
         view.backgroundColor = .white
         setupNavigationBar()
-        title = "봐야 하는 콘텐츠"
     }
     
     // MARK: - func
@@ -74,6 +75,7 @@ final class MainUnwatchedViewController: BaseViewController {
     }
     
     private func setupNavigationBar() {
+        title = "봐야 하는 콘텐츠"
         setupBaseNavigationBar(backgroundColor: .havitWhite,
                                titleColor: .primaryBlack,
                                isTranslucent: false,
@@ -93,6 +95,7 @@ extension MainUnwatchedViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: ContentsCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+        cell.backgroundColor = .white
         return cell
     }
 }
