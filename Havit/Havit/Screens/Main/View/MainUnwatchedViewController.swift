36de2873bb645ca21f4e49922d52b26de849eaf0
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-01-19 18:39:39 +0900

    [ADD] UnWatchedViewController 추가(#120)

---
+++ Havit/Havit/Screens/Main/View/MainUnwatchedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainUnwatchedViewController.swift
+//  Havit
+//
+//  Created by SHIN YOON AH on 2022/01/19.
+//
+
+import UIKit
+
+import SnapKit
+
+final class MainUnwatchedViewController: BaseViewController {
+
+}
