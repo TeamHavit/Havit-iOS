e84016a78db4aaaeaddbf13310a033db7ef91bae
Noah <noa316@naver.com>
2022-01-21 06:40:32 +0900

    [FEAT] Share Extension 초기 API Flow 연결 (#197) - 사용자의 카테고리가 서버에 있는 경우, 없는 경우에 따른 Share Extension 초기화면 분기처리

--- Havit/ShareExtension/EntryNavigationViewController.swift
+++ Havit/ShareExtension/EntryNavigationViewController.swift
@@ -8,4 +8,64 @@
 import UIKit
 
 @objc(EntryNavigationViewController)
-final class EntryNavigationViewController: UINavigationController { }
+final class EntryNavigationViewController: UINavigationController {
+    
+    let categoryService: CategorySeriviceable = CategoryService(apiService: APIService(), environment: .development)
+    
+    var categories: [Category] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getCategory()
+        configUI()
+    }
+    
+    private func configUI() {
+        view.backgroundColor = .white
+        setupBaseNavigationBar()
+    }
+    
+    private func getCategory() {
+        Task {
+            do {
+                let categories = try await categoryService.getCategory()
+                if let categories = categories, !categories.isEmpty {
+                    self.categories = categories
+                } else {
+                    self.categories = []
+                }
+                branchCategoryNavigate()
+            } catch APIServiceError.serverError {
+                Logger.debugDescription("serverError")
+            } catch APIServiceError.clientError(let message) {
+                if let message = message {
+                    Logger.debugDescription("clientError:\(message)")
+                }
+            }
+        }
+    }
+    
+    private func branchCategoryNavigate() {
+        if categories.isEmpty {
+            let categoryEmptyViewController = CategoryEmptyViewController()
+            self.pushViewController(categoryEmptyViewController, animated: true)
+        } else {
+            let selectCategoryViewControllr = SelectCategoryViewController()
+            selectCategoryViewControllr.categories = self.categories
+            self.pushViewController(selectCategoryViewControllr, animated: true)
+        }
+    }
+    
+    func setupBaseNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        let font = UIFont.font(.pretendardBold, ofSize: 16)
+        appearance.backgroundColor = .white
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.havitGray, .font: font]
+        appearance.shadowColor = .clear
+        
+        navigationBar.standardAppearance = appearance
+        navigationBar.compactAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
+        navigationBar.isTranslucent = false
+    }
+}
