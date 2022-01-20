062b9ae29ca751ace4a59842df47b7945c78b13b
Noah <noa316@naver.com>
2022-01-20 16:14:52 +0900

    [FEAT] Category Collection View Cell 레이아웃 구현 (#149)

---
+++ Havit/ShareExtension/Screens/Category/View/Cell/CategoryCollectionViewCell.swift
@@ -0,0 +1,94 @@
+//
+//  CategoryCollectionViewCell.swift
+//  ShareExtension
+//
+//  Created by Noah on 2022/01/20.
+//
+
+import UIKit
+
+final class CategoryCollectionViewCell: BaseCollectionViewCell {
+
+    // MARK: - property
+    
+    private let categoryImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiteral.imgCategoryNone
+        return imageView
+    }()
+
+    private let categoryTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .font(.pretendardMedium, ofSize: 14)
+        return label
+    }()
+
+    private lazy var checkIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiteral.iconcheck
+        return imageView
+    }()
+
+    // MARK: - init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForReuse() {
+        categoryImageView.image = nil
+        categoryTitleLabel.text = ""
+        checkIcon.isHidden = true
+        backgroundColor = .purpleCategory
+    }
+
+    // MARK: - func
+    
+    override func render() {
+        addSubViews([categoryImageView,
+                     categoryTitleLabel,
+                     checkIcon])
+
+        categoryImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(7)
+            $0.width.height.equalTo(36)
+        }
+
+        categoryTitleLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(categoryImageView.snp.trailing).offset(7)
+        }
+        
+        checkIcon.snp.makeConstraints {
+            $0.centerY.equalTo(categoryImageView)
+            $0.trailing.equalToSuperview().inset(19)
+        }
+    }
+
+    override func configUI() {
+        checkIcon.isHidden = true
+        backgroundColor = .purpleCategory
+        layer.cornerRadius = 6
+    }
+
+    func update(data: CategoryListData) {
+        categoryImageView.image = UIImage(named: data.categoryImageName)
+        categoryTitleLabel.text = "\(data.categoryTitle)"
+    }
+    
+    func didSelect() {
+        backgroundColor = .purple002
+        checkIcon.isHidden = false
+    }
+    
+    func didUnSelect() {
+        backgroundColor = .purpleCategory
+        checkIcon.isHidden = true
+    }
+}
