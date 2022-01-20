745281f8026621919c4795f12c60ef6cddd758a0
beansBin <beans_bin@naver.com>
2022-01-20 17:59:14 +0900

    [DEL] CategoryContents requied init 생성자 삭제

--- Havit/Havit/Screens/CategoryContents/View/Components/Cells/CategoryPanModalTableViewCell.swift
+++ Havit/Havit/Screens/CategoryContents/View/Components/Cells/CategoryPanModalTableViewCell.swift
@@ -29,11 +29,6 @@ class CategoryPanModalTableViewCell: BaseTableViewCell {
         return view
     }()
     
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func render() {
         contentView.addSubViews([cellLabel, border, cellImageView])
         
--- Havit/Havit/Screens/CategoryContents/View/Components/Cells/MorePanModalTableViewCell.swift
+++ Havit/Havit/Screens/CategoryContents/View/Components/Cells/MorePanModalTableViewCell.swift
@@ -30,11 +30,6 @@ class MorePanModalTableViewCell: BaseTableViewCell {
         return label
     }()
     
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func render() {
         contentView.addSubViews([cellImageView, cellLabel, borderView])
         
