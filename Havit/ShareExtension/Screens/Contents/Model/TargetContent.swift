a583ff29c55ff0eddf1cce82bcb165f9dd4fd6ab
Noah <noa316@naver.com>
2022-01-21 18:29:54 +0900

    [FEAT] contents 저장 flow 데이터 전달 (#221) - EntryNavigationViewController.swift: 서버에서 받아온 크롤링 contents 다음 뷰 컨트롤러로 전달기능 구현 - TargetContent.swift : User가 저장하고 싶은 content data를 담을 model 구현

---
+++ Havit/ShareExtension/Screens/Contents/Model/TargetContent.swift
@@ -0,0 +1,15 @@
+//
+//  TargetContent.swift
+//  ShareExtension
+//
+//  Created by Noah on 2022/01/21.
+//
+
+import Foundation
+
+struct TargetContent {
+    var title: String?
+    var description: String?
+    var ogImage: String?
+    var ogUrl: String?
+}
